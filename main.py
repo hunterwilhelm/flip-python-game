a15002dd17d81394f30e8be2f60c03e82a0a1d68
Hunter Wilhelm <yeskindofday@gmail.com>
2021-05-20 19:42:51 -0600

    Art and control

--- main.py
+++ main.py
@@ -11,7 +11,15 @@ import arcade
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Starting Template"
+SCREEN_TITLE = "Flip Game"
+
+# scaling
+CHARACTER_SCALING = 1
+# Movement speed of player, in pixels per frame
+PLAYER_MOVEMENT_SPEED = 5
+GRAVITY = .5
+PLAYER_JUMP_SPEED = 10
+TILE_SCALING = 1
 
 
 class MyGame(arcade.Window):
@@ -26,49 +34,101 @@ class MyGame(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
 
-        arcade.set_background_color(arcade.color.AMAZON)
+        arcade.set_background_color(arcade.color.WHITE)
 
-        # If you have sprite lists, you should create them here,
-        # and set them to None
+        self.wall_list = None
+        self.player_list = None
+        self.ladder_list = None
+
+        # Separate variable that holds the player sprite
+        self.player_sprite = None
+
+        # Better Controls
+        self.left_pressed = False
+        self.right_pressed = False
+        self.up_pressed = False
+        self.down_pressed = False
+
+
+        # Our 'physics' engine
+        self.physics_engine = None
+
 
     def setup(self):
         """ Set up the game variables. Call to re-start the game. """
+        # Create the Sprite lists
+        self.player_list = arcade.SpriteList()
+        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
+
         # Create your sprites and sprite lists here
-        pass
+        image_source = "assets/sprites/Owlet_Monster/Owlet_Monster.png"
+        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
+        self.player_sprite.center_x = 64
+        self.player_sprite.center_y = 128
+        self.player_list.append(self.player_sprite)
+
+        # Create the physics engine
+
+        self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite,
+                                                             self.wall_list,
+                                                             gravity_constant=GRAVITY,
+                                                             ladders=self.ladder_list)
+
+        # Create the ground
+        # This shows using a loop to place multiple sprites horizontally
+        for x in range(0, 1250, 64):
+            wall = arcade.Sprite("assets/sprites/Blocks/block.png", TILE_SCALING)
+            wall.center_x = x
+            wall.center_y = 32
+            self.wall_list.append(wall)
 
     def on_draw(self):
         """
         Render the screen.
         """
 
-        # This command should happen before we start drawing. It will clear
-        # the screen to the background color, and erase what we drew last frame.
+        # Clear the screen to the background color
         arcade.start_render()
 
-        # Call draw() on all your sprite lists below
+        self.player_list.draw()
+        self.wall_list.draw()
 
     def on_update(self, delta_time):
-        """
-        All the logic to move, and the game logic goes here.
-        Normally, you'll call update() on the sprite lists that
-        need it.
-        """
-        pass
+        """ Movement and game logic """
+        # Calculate speed based on the keys pressed
+        self.player_sprite.change_x = 0
 
-    def on_key_press(self, key, key_modifiers):
-        """
-        Called whenever a key on the keyboard is pressed.
+        if self.left_pressed and not self.right_pressed:
+            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        elif self.right_pressed and not self.left_pressed:
+            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
 
-        For a full list of keys, see:
-        https://arcade.academy/arcade.key.html
-        """
-        pass
+        # Move the player with the physics engine
+        self.physics_engine.update()
 
-    def on_key_release(self, key, key_modifiers):
-        """
-        Called whenever the user lets off a previously pressed key.
-        """
-        pass
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            if self.physics_engine.can_jump():
+                self.player_sprite.change_y = PLAYER_JUMP_SPEED
+        elif key == arcade.key.DOWN:
+            self.down_pressed = True
+        elif key == arcade.key.LEFT:
+            self.left_pressed = True
+        elif key == arcade.key.RIGHT:
+            self.right_pressed = True
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+
+        if key == arcade.key.DOWN:
+            self.down_pressed = False
+        elif key == arcade.key.LEFT:
+            self.left_pressed = False
+        elif key == arcade.key.RIGHT:
+            self.right_pressed = False
+
 
     def on_mouse_motion(self, x, y, delta_x, delta_y):
         """
